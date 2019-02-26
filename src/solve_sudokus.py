be06922d1261fa6a72c035524769e01dd9a3f0cf
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-02-26 16:33:58 +0100

    monitor all algs

--- src/solve_sudokus.py
+++ src/solve_sudokus.py
@@ -6,13 +6,9 @@ from sat import monitor_runs, Algorithm
 
 def main():
     fpaths = glob.glob(os.path.join(os.getcwd(), 'data', 'dimacs', '*', '*.in'))
-    # logging.warning('solving sudokus')
-    alg = Algorithm.RANDOM
-    # logging.warning(alg.name)
-    # df = 
-    monitor_runs(fpaths, alg=alg, loglvl=logging.ERROR)
-    # solved = df['solved'].tolist()
-    # logging.error(f"Solved: {solved.count(True)}, Unsolved: {solved.count(False)}")
+    for alg in Algorithm:
+        logging.error(alg.name)
+        monitor_runs(fpaths, alg=alg, loglvl=logging.ERROR)
 
 if __name__ == "__main__":
     main()
